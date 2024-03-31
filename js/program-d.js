2350c0aec11b565733005c3e00abd080c7a34d97
AssaultX492 <153366223+AssaultX492@users.noreply.github.com>
2024-03-31 14:38:20 +0300

    Main-design-ready

--- js/program-d.js
+++ js/program-d.js
@@ -1,3 +1,47 @@
+function outlineOn(element) {
+    let paragraphs = element.querySelectorAll('.bl-btn-desc p');
 
+    paragraphs.forEach(function (paragraph) {
+        paragraph.style.color = 'white'; 
+    });
 
+    if (element.id === 'bl-1') {
+        element.querySelector('.bl-btn-icon img').src = './img/controller-white.png';
+    } else if (element.id === 'bl-2') {
+        element.querySelector('.bl-btn-icon img').src = './img/liderboard-white.png';
+    } else if (element.id === 'bl-3') {
+        element.querySelector('.bl-btn-icon img').src = './img/history-white.png';
+    } else if (element.id === 'bl-4') {
+        element.querySelector('.bl-btn-icon img').src = './img/update-white.png';
+    } else if (element.id === 'bl-5') {
+        element.querySelector('.bl-btn-icon img').src = './img/question-white.png';
+    } else if (element.id === 'bl-6') {
+        element.querySelector('.bl-btn-icon img').src = './img/dev-white.png';
+    }
 
+    
+    
+}
+
+function outlineOff(element) {
+    let paragraphs = element.querySelectorAll('.bl-btn-desc p');
+
+    paragraphs.forEach(function (paragraph) {
+        paragraph.style.color = '';
+    });
+
+    if (element.id === 'bl-1') {
+        element.querySelector('.bl-btn-icon img').src = './img/controller.png';
+    } else if (element.id === 'bl-2') {
+        element.querySelector('.bl-btn-icon img').src = './img/liderboard.png';
+    } else if (element.id === 'bl-3') {
+        element.querySelector('.bl-btn-icon img').src = './img/history.png';
+    } else if (element.id === 'bl-4') {
+        element.querySelector('.bl-btn-icon img').src = './img/update.png';
+    } else if (element.id === 'bl-5') {
+        element.querySelector('.bl-btn-icon img').src = './img/question.png';
+    } else if (element.id === 'bl-6') {
+        element.querySelector('.bl-btn-icon img').src = './img/dev.png';
+    }
+
+}
