915574bfedec2d0abc657b1e5bfb5a48703bebd1
AssaultX492 <153366223+AssaultX492@users.noreply.github.com>
2024-05-12 13:43:43 +0300

    f2

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
 
-document.getElementById("appVersion").textContent = "0.0.8b"
+document.getElementById("appVersion").textContent = "0.0.6b"
 sessionStorage.setItem("version", document.getElementById("appVersion").textContent);
